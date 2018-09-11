6f610d334f82af1ac8fa72bfb93e280b4d18c577
Rick Yang <rick68@gmail.com>
2018-09-11 21:05:15 +0800

    add a file assgn_st.rs

--- Chapter 4/assgn_st.rs
+++ Chapter 4/assgn_st.rs
@@ -3,25 +3,26 @@
 use std::io;
 use std::io::prelude::*;
 
-struct inflatable {
+struct Inflatable {
     name: &'static str,
+    #[allow(unused)]
     volume: f32,
     price: f64,
 }
 
 fn main() -> io::Result<()> {
     let mut stdout: io::Stdout = io::stdout();
-    let bouquet: inflatable = inflatable {
+    let bouquet: Inflatable = Inflatable {
         name: "sunflowers",
         volume: 0.20,
         price: 12.49,
     };
-    let choice: inflatable;
+    let choice: Inflatable;
 
     write!(stdout, "bouquet: {} for $", bouquet.name);
     write!(stdout, "{}\n", bouquet.price);
 
-    choice = bouquet;   // move one structre to another
+    choice = bouquet; // move one structre to another
     write!(stdout, "choice: {} for $", choice.name);
     write!(stdout, "{}\n", choice.price);
 
